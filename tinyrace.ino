0aa535f9880268cf37412cdeb293ce33b6dfc6a3
Thorben Kroeger <dev@thorben.net>
2022-12-07 10:44:16 +0100

    refactor

--- tinyrace.ino
+++ tinyrace.ino
@@ -24,6 +24,7 @@ unsigned int iters[2];
 unsigned long last_led_update;
 uint8_t led_number;
 bool display_speeds[2];
+bool in_menu;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -41,6 +42,44 @@ Button2 button_down;
 #define BLINK_LEDS
 #define DEBUG_PRINTS
 
+void leds_blink(int led_number) {
+  for (uint8_t lane = 0; lane < 2; ++lane) {
+    for (uint8_t i = 0; i < LANE_LEDS; ++i) {
+#ifdef PATTERN_1
+      if (i == led_number)
+        leds[LANE_LEDS * lane + i] =
+            (lane == 0) ? CRGB(4, 0, 0) : CRGB(0, 4, 0);
+      else
+        leds[LANE_LEDS * lane + i] =
+            (lane == 0) ? CRGB(0, 0, 4) : CRGB(4, 4, 0);
+#else
+      if (i % 2 == led_number)
+        leds[LANE_LEDS * lane + i] =
+            (lane == 0) ? CRGB(4, 0, 0) : CRGB(0, 4, 0);
+      else
+        leds[LANE_LEDS * lane + i] =
+            (lane == 0) ? CRGB(0, 4, 0) : CRGB(0, 0, 4);
+#endif
+    }
+  }
+}
+
+void leds_display_winner() {
+  CRGB lane_a_color, lane_b_color;
+  if (lane_micros[0][1] > lane_micros[1][1]) {
+    lane_a_color = CRGB(0, 128, 0);
+    lane_b_color = CRGB(128, 0, 0);
+  } else {
+    lane_a_color = CRGB(128, 0, 0);
+    lane_b_color = CRGB(0, 128, 0);
+  }
+  for (uint8_t lane = 0; lane < 2; ++lane) {
+    for (uint8_t i = 0; i < LANE_LEDS; ++i) {
+      leds[LANE_LEDS * lane + i] = (lane == 0) ? lane_a_color : lane_b_color;
+    }
+  }
+}
+
 void full_reset() {
   for (uint8_t lane = 0; lane < 2; ++lane) {
     for (uint8_t pos = 0; pos < 2; ++pos) {
@@ -53,6 +92,7 @@ void full_reset() {
   }
   last_led_update = 0;
   led_number = 0;
+  in_menu = false;
 
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -114,128 +154,7 @@ void debug_race() {
   delay(50);
 }
 
-void loop() {
-#ifdef DEBUG_RACE
-  debug_race();
-#else
-
-  if ((lane_passed[0][1] &&
-       micros() > race_timeout_usecs + lane_micros[0][1]) ||
-      (lane_passed[1][1] &&
-       micros() > race_timeout_usecs + lane_micros[1][1])) {
-    full_reset();
-    Serial.println("Reset");
-
-    Serial.println(lane_passed[0][0]);
-  }
-
-#ifdef BLINK_LEDS
-
-  if (last_led_update + 1000 < millis()) {
-    unsigned long start_update = micros();
-
-    if (lane_micros[0][1] > 0 && lane_micros[1][1] > 0) {
-      Serial.println("red/green leds");
-      CRGB lane_a_color, lane_b_color;
-      if (lane_micros[0][1] > lane_micros[1][1]) {
-        lane_a_color = CRGB(0, 128, 0);
-        lane_b_color = CRGB(128, 0, 0);
-      } else {
-        lane_a_color = CRGB(128, 0, 0);
-        lane_b_color = CRGB(0, 128, 0);
-      }
-      for (uint8_t lane = 0; lane < 2; ++lane) {
-        for (uint8_t i = 0; i < LANE_LEDS; ++i) {
-          leds[LANE_LEDS * lane + i] =
-              (lane == 0) ? lane_a_color : lane_b_color;
-        }
-      }
-    } else {
-      Serial.println("blinky leds");
-      for (uint8_t lane = 0; lane < 2; ++lane) {
-        for (uint8_t i = 0; i < LANE_LEDS; ++i) {
-#ifdef PATTERN_1
-          if (i == led_number)
-            leds[LANE_LEDS * lane + i] =
-                (lane == 0) ? CRGB(4, 0, 0) : CRGB(0, 4, 0);
-          else
-            leds[LANE_LEDS * lane + i] =
-                (lane == 0) ? CRGB(0, 0, 4) : CRGB(4, 4, 0);
-#else
-          if (i % 2 == led_number)
-            leds[LANE_LEDS * lane + i] =
-                (lane == 0) ? CRGB(4, 0, 0) : CRGB(0, 4, 0);
-          else
-            leds[LANE_LEDS * lane + i] =
-                (lane == 0) ? CRGB(0, 4, 0) : CRGB(0, 0, 4);
-#endif
-        }
-      }
-    }
-
-    FastLED.show();
-    unsigned long end_update = micros();
-    last_led_update = millis();
-
-#ifdef DEBUG_UPDATE
-    Serial.print("update time LEDs [usecs]: ");
-    Serial.print(end_update - start_update);
-    // Serial.print(", led_number=");
-    // Serial.print(led_number);
-    Serial.println("");
-#endif
-
-#ifdef PATTERN_1
-    led_number = (led_number + 1) % NUM_LEDS;
-#else
-    led_number = (led_number + 1) % 2;
-#endif
-  }
-#endif
-
-#if 1
-  button_left.loop();
-  button_right.loop();
-  button_up.loop();
-  button_down.loop();
-  if (button_left.wasPressed()) {
-    switch (button_left.read()) {
-    case single_click:
-      Serial.println("left single");
-      break;
-    case long_click:
-      Serial.println("left long");
-    }
-  }
-  if (button_right.wasPressed()) {
-    switch (button_right.read()) {
-    case single_click:
-      Serial.println("right single");
-      break;
-    case long_click:
-      Serial.println("right long");
-    }
-  }
-  if (button_up.wasPressed()) {
-    switch (button_up.read()) {
-    case single_click:
-      Serial.println("up single");
-      break;
-    case long_click:
-      Serial.println("up long");
-    }
-  }
-  if (button_down.wasPressed()) {
-    switch (button_down.read()) {
-    case single_click:
-      Serial.println("down single");
-      break;
-    case long_click:
-      Serial.println("down long");
-    }
-  }
-#endif
-
+void speed_measure_loop() {
   for (uint8_t lane = 0; lane < 2; ++lane) {
     if (!lane_passed[lane][0]) {
       lane_values[lane][0] = analogRead(lane_sensors[lane][0]);
@@ -322,5 +241,111 @@ void loop() {
       }
     }
   }
+}
+
+void loop() {
+#ifdef DEBUG_RACE
+  debug_race();
+#else
+
+  if ((lane_passed[0][1] &&
+       micros() > race_timeout_usecs + lane_micros[0][1]) ||
+      (lane_passed[1][1] &&
+       micros() > race_timeout_usecs + lane_micros[1][1])) {
+    full_reset();
+    Serial.println("Reset");
+  }
+
+#ifdef BLINK_LEDS
+
+  if (last_led_update + 1000 < millis()) {
+    unsigned long start_update = micros();
+
+    if (lane_micros[0][1] > 0 && lane_micros[1][1] > 0) {
+      leds_display_winner();
+    } else {
+      leds_blink(led_number);
+    }
+
+    FastLED.show();
+    unsigned long end_update = micros();
+    last_led_update = millis();
+
+#ifdef DEBUG_UPDATE
+    Serial.print("update time LEDs [usecs]: ");
+
+    Serial.print(end_update - start_update);
+    Serial.println("");
+#endif
+
+#ifdef PATTERN_1
+    led_number = (led_number + 1) % NUM_LEDS;
+#else
+    led_number = (led_number + 1) % 2;
+#endif
+  }
+#endif
+
+  bool prev_in_menu = in_menu;
+
+#if 1
+  button_left.loop();
+  button_right.loop();
+  button_up.loop();
+  button_down.loop();
+  if (button_left.wasPressed()) {
+    switch (button_left.read()) {
+    case single_click:
+      Serial.println("left single");
+      break;
+    case long_click:
+      Serial.println("left long");
+      in_menu = !in_menu;
+    }
+  }
+  if (button_right.wasPressed()) {
+    switch (button_right.read()) {
+    case single_click:
+      Serial.println("right single");
+      break;
+    case long_click:
+      Serial.println("right long");
+    }
+  }
+  if (button_up.wasPressed()) {
+    switch (button_up.read()) {
+    case single_click:
+      Serial.println("up single");
+      break;
+    case long_click:
+      Serial.println("up long");
+    }
+  }
+  if (button_down.wasPressed()) {
+    switch (button_down.read()) {
+    case single_click:
+      Serial.println("down single");
+      break;
+    case long_click:
+      Serial.println("down long");
+    }
+  }
+#endif
+
+  if (in_menu) {
+    if (!prev_in_menu) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Menu");
+    } else if (button_up.read() == single_click) {
+      lcd.setCursor(0, 1);
+      lcd.print("bla");
+    }
+  } else if (!in_menu && prev_in_menu) {
+    full_reset();
+  } else {
+    speed_measure_loop();
+  }
+
 #endif
 }
